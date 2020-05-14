640c410dffc80809a2079768561f497b61722106
Psilon <pzixel@gmail.com>
2020-05-14 20:46:14 +0300

    compiles part 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+//! `Middleware` for conditionally enables another middleware.
 use std::task::{Context, Poll};
 
 use actix_service::{Service, Transform};
@@ -6,7 +7,7 @@ use std::sync::Arc;
 
 pub struct Condition<T, F> {
     trans: Arc<T>,
-    enable: F,
+    enable: F
 }
 
 impl<T, F> Condition<T, F> {
@@ -18,56 +19,51 @@ impl<T, F> Condition<T, F> {
 impl<S, T, F> Transform<S> for Condition<T, F>
     where
         S: Service + 'static,
-        T: Transform<S, Request = S::Request, Response = S::Response, Error = S::Error>,
+        T: Transform<S, Request = S::Request, Response = S::Response, Error = S::Error> + 'static,
         T::Future: 'static,
         T::InitError: 'static,
-        T::Transform: 'static,
-        <T as Transform<S>>::Transform: Transform<S>,
-        F : Clone
+        T::Transform: 'static
 {
     type Request = S::Request;
     type Response = S::Response;
-    type Error = S::Error;
-    type Transform = ConditionMiddleware<T::Transform, F, S>;
+    type Error = T::Error;
     type InitError = T::InitError;
+    type Transform = ConditionMiddleware<T, S>;
     type Future = LocalBoxFuture<'static, Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
-        Either::Right(ok(ConditionMiddleware {
-            transform: self.trans.clone(),
-            enable: self.enable.clone(),
-            service: Some(service)
-        }))
+        ok(ConditionMiddleware {
+            trans: self.trans.clone(),
+            service
+        })
             .boxed_local()
     }
 }
 
-pub struct ConditionMiddleware<T, F, S> {
-    transform: Arc<T>,
-    enable: F,
-    service: Option<S>
+pub struct ConditionMiddleware<T, S> {
+    trans: Arc<T>,
+    service: S
 }
 
-impl<T, F, S> Service for ConditionMiddleware<T, F, S>
+impl<T, S> Service for ConditionMiddleware<T, S>
     where
-        T: Transform<S>,
-        S: Service<Request = T::Request, Response = T::Response, Error = T::Error>,
+        S: Service + 'static
 {
-    type Request = T::Request;
-    type Response = T::Response;
-    type Error = T::Error;
-    type Future = Either<T::Future, S::Future>;
+    type Request = S::Request;
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = Either<S::Future, S::Future>;
 
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
     }
 
-    fn call(&mut self, req: T::Request) -> Self::Future {
-        if true {
-            let service = self.service.take();
-            self.transform.new_transform(service.unwrap())
-        } else {
-            self.service.unwrap().call()
-        }
+    fn call(&mut self, req: S::Request) -> Self::Future {
+        unimplemented!()
+        // use ConditionMiddleware::*;
+        // match self {
+        //     Enable(service) => Either::Left(service.call(req)),
+        //     Disable(service) => Either::Right(service.call(req)),
+        // }
     }
 }
